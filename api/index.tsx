620cb9640c4afaa73e010dedb008ff26154f5765
artlu99 <alu99.sm@gmail.com>
2024-02-29 20:31:01 -0500

    initial commit

---
+++ api/index.tsx
@@ -0,0 +1,64 @@
+import { Button, Frog } from 'frog'
+import { handle } from 'frog/vercel'
+
+// Uncomment to use Edge Runtime.
+export const config = {
+  runtime: 'edge',
+}
+
+export const app = new Frog({
+  basePath: '/api',
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: 'https://api.hub.wevm.dev',
+  // hubApiUrl: 'https://hub.pinata.cloud',
+})
+
+app.frame('/', (c) => {
+  const { status, frameData, verified } = c
+  const fid = frameData?.fid
+  
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `You are fid ${fid}. I did not make a round-trip to a Hub to verify you`
+            : 'I (will) know who you are'}
+        </div>
+      </div>
+    ),
+    intents: [
+      status === 'response' ? null : <Button value="letmein">Let Me In</Button>,
+    ],
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
